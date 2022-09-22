40f878d56542a8eda7dbabfdac1f51720d64bc87
qys <2667484995@qq.com>
2022-09-22 22:28:23 +0800

    hot-fix02

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!1");
+        System.out.println("hot-fix test！");
     }
 }
