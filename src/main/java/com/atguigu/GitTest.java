268adde02f87c95210ad51fec0ef9f21030297b9
qys <2667484995@qq.com>
2022-09-22 23:07:25 +0800

    master002

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git!1");
         System.out.println("master test01!");
         System.out.println("hot-fix test！");
+        System.out.println("git pull");
     }
 }
