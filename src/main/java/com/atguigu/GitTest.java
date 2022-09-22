f51ef813ccafe53430938293fc67c9f339cad4d2
qys <2667484995@qq.com>
2022-09-22 22:29:59 +0800

    third

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!1");
+        System.out.println("hello git!");
+        System.out.println("master test!");
     }
 }
